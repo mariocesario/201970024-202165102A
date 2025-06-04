54cde740d1c4799d1b0d53f72ce91b6ec8b2a0ed
Mario Cesario <mario.henrique.cesario@gmail.com>
2025-06-03 21:39:47 -0300

    Adicionado inicialização do jogo e posicionamento dos personagens

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,12 @@
 package app;
 
+import game.Game;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Character Duel Game!");
+        // Iniciando o jogo
+        Game game = new Game();
+        game.start();
     }
 }
--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -1,8 +1,29 @@
 package game;
 
+import character.Warrior;
+import character.Archer;
+import player.Player;
+import game.Board;
+
 public class Game {
     public void start() {
         System.out.println("Game started!");
-        // Logic will go here
+
+        
+        Board board = new Board();
+
+        Warrior warrior = new Warrior("Thor");
+        Archer archer = new Archer("Legolas");
+
+       
+       Player player1 = new Player("Mário", warrior, false);
+       Player player2 = new Player("CPU", archer, true);
+
+       
+        board.placeCharacter(warrior, 2, 2);
+        board.placeCharacter(archer, 5, 5);
+
+        
+        board.printBoard();
     }
 }
